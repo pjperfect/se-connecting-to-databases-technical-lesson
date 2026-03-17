90bac4b86e0e3cf7db3125f8cd1125cd66c5773c
Philip Olembo <pjole.kenya@gmail.com>
2026-03-17 16:26:44 +0300

    Technical Lesson: Connecting to SQL Databases

--- connecting-to-db.py
+++ connecting-to-db.py
@@ -1,2 +1,22 @@
 import sqlite3
 import pandas as pd
+
+conn = sqlite3.connect('data.sqlite')
+
+cur = conn.cursor()
+
+df = pd.DataFrame(
+    cur.execute("""SELECT * FROM offices;""").fetchall(),
+    columns = [x[0] for x in cur.description]
+)
+
+# cur.execute("""SELECT name FROM sqlite_master WHERE type = 'table';""")
+# cur.execute("""SELECT * FROM offices;""")
+
+# table_names = cur.fetchall()
+# office_data = cur.fetchall()
+# office_cols = cur.description
+
+print(df)
+
+conn.close()
